413135a66d3a8a30992e91ec94916bdc96ed14b8
Rafael Alves <rbalves.ads@gmail.com>
2022-08-04 16:03:38 -0300

    feat: challenge 3

--- challenge-3.js
+++ challenge-3.js
@@ -1,7 +1,23 @@
 // Desafio 3: Duplicate
 
+/*
+Contar o número de duplicatas.
+
+Escreva uma função que retornará a contagem de caracteres alfabéticos distintos que não diferenciam maiúsculas de minúsculas e dígitos numéricos 
+que ocorrem mais de uma vez na string de entrada. A string de entrada pode conter apenas letras (maiúsculas e minúsculas) e dígitos numéricos.
+*/
+
 function duplicateCount(text) {
-  // Write your code here
+  const formatedText = [...text.toLowerCase()];
+
+  const duplicates = formatedText.filter(
+    (item, index) => formatedText.indexOf(item) !== index
+  );
+
+  return {
+    duplicados: duplicates.length,
+    caracteres: duplicates,
+  };
 }
 
 console.log(duplicateCount("abcde")); // {duplicados: 0, caracteres: []}

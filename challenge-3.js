0d36b45bc050eb287b13e1f3af515c1f64754a7a
Rafael Alves <rbalves.ads@gmail.com>
2022-08-04 16:05:29 -0300

    refactor: remove commment

--- challenge-3.js
+++ challenge-3.js
@@ -1,12 +1,5 @@
 // Desafio 3: Duplicate
 
-/*
-Contar o número de duplicatas.
-
-Escreva uma função que retornará a contagem de caracteres alfabéticos distintos que não diferenciam maiúsculas de minúsculas e dígitos numéricos 
-que ocorrem mais de uma vez na string de entrada. A string de entrada pode conter apenas letras (maiúsculas e minúsculas) e dígitos numéricos.
-*/
-
 function duplicateCount(text) {
   const formatedText = [...text.toLowerCase()];
 

daa8896f279c79c0aa19813f10ec6a29c3ae1b0b
Rafael Alves <rbalves.ads@gmail.com>
2022-08-04 19:53:30 -0300

    feat(challnege-3): create new solution

--- challenge-3.js
+++ challenge-3.js
@@ -1,19 +1,6 @@
 // Desafio 3: Duplicate
 
 function duplicateCount(text) {
-  const formatedText = [...text.toLowerCase()];
-
-  const duplicates = formatedText.filter(
-    (item, index) => formatedText.indexOf(item) !== index
-  );
-
-  return {
-    duplicados: duplicates.length,
-    caracteres: duplicates,
-  };
-}
-
-function newSolution(text) {
   const textToLowerCase = text.toLowerCase();
 
   const distincts = [...new Set([...textToLowerCase])];
@@ -40,7 +27,7 @@ function newSolution(text) {
   );
 }
 
-console.log(newSolution("abcde")); // {duplicados: 0, caracteres: []}
-console.log(newSolution("abcdea")); // {duplicados: 1, caracteres: ['a']}
-console.log(newSolution("aabBcde")); // {duplicados: 2, caracteres: ['a', 'b']}
-console.log(newSolution("aA11")); // {duplicados: 2, caracteres: ['a', 1]}
+console.log(duplicateCount("abcde")); // {duplicados: 0, caracteres: []}
+console.log(duplicateCount("abcdea")); // {duplicados: 1, caracteres: ['a']}
+console.log(duplicateCount("aabBcde")); // {duplicados: 2, caracteres: ['a', 'b']}
+console.log(duplicateCount("aA11")); // {duplicados: 2, caracteres: ['a', 1]}

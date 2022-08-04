810cf15ab2c0b6c666e9bedb438140c6fc93da77
Rafael Alves <rbalves.ads@gmail.com>
2022-08-04 19:13:15 -0300

    feat(challnege-3): create new solution

--- challenge-3.js
+++ challenge-3.js
@@ -13,7 +13,34 @@ function duplicateCount(text) {
   };
 }
 
-console.log(duplicateCount("abcde")); // {duplicados: 0, caracteres: []}
-console.log(duplicateCount("abcdea")); // {duplicados: 1, caracteres: ['a']}
-console.log(duplicateCount("aabBcde")); // {duplicados: 2, caracteres: ['a', 'b']}
-console.log(duplicateCount("aA11")); // {duplicados: 2, caracteres: ['a', 1]}
+function newSolution(text) {
+  const textToLowerCase = text.toLowerCase();
+
+  const distincts = [...new Set([...textToLowerCase])];
+
+  return distincts.reduce(
+    (acc, letter) => {
+      var quantityLetter = [...textToLowerCase].reduce((acc, character) => {
+        return character === letter ? (acc += 1) : acc;
+      }, 0);
+
+      const isDuplicated = quantityLetter === 2;
+
+      if (isDuplicated) {
+        return {
+          ...acc,
+          duplicados: acc.duplicados + 1,
+          caracteres: [...acc.caracteres, letter],
+        };
+      }
+
+      return { ...acc };
+    },
+    { duplicados: 0, caracteres: [] }
+  );
+}
+
+console.log(newSolution("abcde")); // {duplicados: 0, caracteres: []}
+console.log(newSolution("abcdea")); // {duplicados: 1, caracteres: ['a']}
+console.log(newSolution("aabBcde")); // {duplicados: 2, caracteres: ['a', 'b']}
+console.log(newSolution("aA11")); // {duplicados: 2, caracteres: ['a', 1]}

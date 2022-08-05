ce6d2272e1d6b0fb717bbef2ff1740012ea8ea1d
Rafael Alves <rbalves.ads@gmail.com>
2022-08-05 08:43:45 -0300

    refactor(challenge-1): create list with name

--- challenge-1.js
+++ challenge-1.js
@@ -1,7 +1,9 @@
 // Desafio 1: FizzBuzz
 
 function fizzBuzz(number) {
-  return Array.from({ length: number }, (_, i) => i + 1).map((number) => {
+  const numbers = Array.from({ length: number }, (_, i) => i + 1);
+
+  return numbers.map((number) => {
     const Fizz = number % 3 === 0;
     const Buzz = number % 5 === 0;
 
